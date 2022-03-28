6f3982a7b99e2812d7792d2910b32e50df928eeb
Thành Nam <thanhnam.thai01@gmail.com>
2022-03-28 18:31:42 +0700

    done RegisterActivity

--- app/src/main/java/com/toptop/LoginActivity.java
+++ app/src/main/java/com/toptop/LoginActivity.java
@@ -3,12 +3,14 @@ package com.toptop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -43,5 +45,12 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setTranslucentStatusBar();
+
+        TextView register = findViewById(R.id.txtLogin);
+        register.setOnClickListener(v -> {
+                    Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                    startActivity(intent);
+                }
+        );
     }
 }
--- app/src/main/java/com/toptop/RegisterActivity.java
+++ app/src/main/java/com/toptop/RegisterActivity.java
@@ -1,19 +1,16 @@
 package com.toptop;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
-public class SplashScreen extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
     public static void setWindowFlag(Activity activity, final int bits, boolean on) {
         Window win = activity.getWindow();
         WindowManager.LayoutParams winParams = win.getAttributes();
@@ -38,18 +35,11 @@ public class SplashScreen extends AppCompatActivity {
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
-
+        setContentView(R.layout.activity_register);
         setTranslucentStatusBar();
-
-        new Handler().postDelayed(() -> {
-            // Start the next activity
-            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-            startActivity(intent);
-            finish();
-        }, 1000);
     }
 }
