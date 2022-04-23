23ba7e66795fda3b1a4d864319d3dd131a81a3ef
ngoctrung0962 <84114824+ngoctrung0962@users.noreply.github.com>
2022-04-23 15:23:45 +0700

    EditUserInfo

---
+++ app/src/main/java/com/toptop/formEditIProfile.java
@@ -0,0 +1,14 @@
+package com.toptop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class formEditIProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_edit_iprofile);
+    }
+}
