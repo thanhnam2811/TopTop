e28b57187acdb2367ba903ef8f3d2f98d0dfbd9e
Thành Nam <thanhnam.thai01@gmail.com>
2022-05-28 09:50:15 +0700

    fix delay when start, change icon design, remove some unnecessary image

--- app/src/main/java/com/toptop/service/NotificationService.java
+++ app/src/main/java/com/toptop/service/NotificationService.java
@@ -14,31 +14,21 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.RemoteInput;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 import com.toptop.MainActivity;
 import com.toptop.R;
 import com.toptop.WatchProfileActivity;
 import com.toptop.WatchVideoActivity;
 import com.toptop.models.User;
 import com.toptop.models.Video;
-import com.toptop.utils.firebase.FirebaseUtil;
 import com.toptop.utils.firebase.NotificationFirebase;
 import com.toptop.utils.firebase.VideoFirebase;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class NotificationService extends Service {
 
@@ -62,10 +52,7 @@ public class NotificationService extends Service {
 			notificationUnseen(MainActivity.getCurrentUser());
 		}
 
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-			startMyOwnForeground();
-		else
-			startForeground(1, new Notification());
+		startMyOwnForeground();
 
 		return START_NOT_STICKY;
 	}
@@ -82,7 +69,7 @@ public class NotificationService extends Service {
 
 		NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
 		Notification notification = notificationBuilder.setOngoing(true)
-				.setSmallIcon(R.drawable.logo_toptop)
+				.setSmallIcon(R.drawable.ic_logo)
 				.setContentTitle("App is running in background")
 				.setPriority(NotificationManager.IMPORTANCE_MIN)
 				.setCategory(Notification.CATEGORY_SERVICE)
@@ -94,7 +81,7 @@ public class NotificationService extends Service {
 	private void notificationUnseen(User user) {
 		NotificationFirebase.getNotificationByUsername(user.getUsername(), notifications -> {
 			for (com.toptop.models.Notification notification : notifications) {
-				if (notification.getSeen() == false) {
+				if (!notification.getSeen()) {
 					sendNotification(notification);
 					notification.setSeen(true);
 					NotificationFirebase.updateNotification(notification);
@@ -107,14 +94,12 @@ public class NotificationService extends Service {
 		NotificationManager notificationManager =
 				(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 //		create channel notification
-		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-			@SuppressLint("WrongConstant")
-			NotificationChannel notificationChannel = new NotificationChannel("my_notification", "n_channel", NotificationManager.IMPORTANCE_MAX);
-			notificationChannel.setDescription("description");
-			notificationChannel.setName("Channel Name");
-			assert notificationManager != null;
-			notificationManager.createNotificationChannel(notificationChannel);
-		}
+		@SuppressLint("WrongConstant")
+		NotificationChannel notificationChannel = new NotificationChannel("my_notification", "n_channel", NotificationManager.IMPORTANCE_MAX);
+		notificationChannel.setDescription("description");
+		notificationChannel.setName("Channel Name");
+		assert notificationManager != null;
+		notificationManager.createNotificationChannel(notificationChannel);
 
 //		create notification
 		NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "my_notification");
@@ -131,7 +116,7 @@ public class NotificationService extends Service {
 							PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
 							Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 							NotificationCompat.Builder notificationBuilderVideo = new NotificationCompat.Builder(this)
-									.setSmallIcon(R.drawable.logo_toptop)
+									.setSmallIcon(R.drawable.ic_logo)
 									.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_liked))
 									.setContentTitle("Bạn có một lượt thích mới")
 									.setContentText(notification.getContent())
@@ -143,7 +128,6 @@ public class NotificationService extends Service {
 									.setOnlyAlertOnce(true)
 									.setChannelId("my_notification")
 									.setColor(Color.parseColor("#3F5996"));
-							assert notificationManager != null;
 							notificationManager.notify(NOTIFICATION_LIKE_ID, notificationBuilderVideo.build());
 						}
 					}, error -> {
@@ -159,7 +143,7 @@ public class NotificationService extends Service {
 			Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 			//notification for follow
 			notificationBuilder = new NotificationCompat.Builder(this)
-					.setSmallIcon(R.drawable.logo_toptop)
+					.setSmallIcon(R.drawable.ic_logo)
 					.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_following))
 					.setContentTitle("Bạn có một người theo dõi mới")
 					.setContentText(notification.getContent())
@@ -172,7 +156,6 @@ public class NotificationService extends Service {
 					.setChannelId("my_notification")
 					.setColor(Color.parseColor("#3F5996"));
 			//.setProgress(100,50,false);
-			assert notificationManager != null;
 			notificationManager.notify(NOTIFICATION_FOLLOW_ID, notificationBuilder.build());
 		} else {
 			// event click notification to open activity
@@ -186,7 +169,7 @@ public class NotificationService extends Service {
 			System.out.println("notification.getType() = " + notification.getType());
 			//Create notification builder
 			notificationBuilder = new NotificationCompat.Builder(this)
-					.setSmallIcon(R.drawable.logo_toptop)
+					.setSmallIcon(R.drawable.ic_logo)
 					.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_comment))
 					.setContentTitle("Bạn có một bình luận mới")
 					.setContentText(notification.getContent())
